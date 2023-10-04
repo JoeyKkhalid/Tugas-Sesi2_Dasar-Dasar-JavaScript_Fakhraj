4275fd6095afdcf97b385aa3f94f279b06d8ac39
JoeyKkhalid <114896032+JoeyKkhalid@users.noreply.github.com>
2023-10-04 23:09:38 +0700

    Add files via upload

---
+++ Tugas.js
@@ -0,0 +1,8 @@
+const firstName = "Muhammad Fakhraj";
+const lastName = "Jaelani";
+const age = 20;
+const isMarried = false;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried); 
